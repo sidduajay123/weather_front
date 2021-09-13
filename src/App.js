c7e0ecada89fb32b8bcf25f50d56028d4802ebba
sidduajay123 <sidduajay123@gmail.com>
2021-09-13 10:05:00 +0530

    weather frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+// import { useEffect, useState } from 'react';
+import Search from './components/Search';
 
-function App() {
+
+const App = () => {
+  // const [tasks, setTasks] = useState([])
+
+  // useEffect(() => {
+  //   const getTasks = async () =>{
+  //     const tasksFromServer = await fetchTasks()
+  //     // setTasks(tasksFromServer)
+  //   }
+  //   getTasks()
+  // },[])
+
+  // const [text, setText] = useState('')
+  //   const onSubmit = (e) => {
+  //       e.preventDefault()
+    
+  //       if (!text) {
+  //         alert('Please add a task')
+  //         return
+  //       }
+    
+    
+  //       setText('')
+  //     }
+
+  // const fetchTasks = async (searchdata) => {
+  //   const res = await fetch('http://localhost:8080/api', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(searchdata),
+  //     })
+  
+  //   const data = await res.json()
+  
+  //   console.log(data)
+  //   return data
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="App">
+            <Search />
+          </div>
   );
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Search.js
@@ -0,0 +1,157 @@
+import React from "react";
+export default class Forms extends React.Component{
+
+    constructor()
+    {
+        super()
+        this.state = {
+            city:null,
+            result:{
+                main :{}
+            },
+            
+        }
+    }
+
+    submit(){
+        let searchSubmit = {
+            city:this.state.city
+        }
+
+        var formData = new FormData();
+        formData.append('city', this.state.city);
+
+        fetch('http://localhost:8080/api',
+            {
+                method:'POST',
+                headers:{
+                    'Content-type' : 'application/json'
+                },
+                body:JSON.stringify(searchSubmit)
+            }).then(r=>r.json()).then(res=>{
+                if(res.status_code === 200){
+                    this.setState({result:res.result})
+                }
+                
+                console.log(this.state.result)
+                console.log(this.state.result.main)
+                console.log(this.state.result.main.temp)
+            })
+
+            // const data = res.json()
+            
+        
+    }
+
+    // weatherResult(){
+    //     if(this.state.result !== null){
+    //          (
+    //             <div>Weather Result</div>
+    //         );
+    //     }
+    // }
+
+    render()
+    {
+        return(
+            <div>
+                <div>
+                    Search weather By city Name
+                </div>
+                <div className='add-form' >
+                    <br/>
+                    <div className='form-control'>
+                        <label>Enter City</label>&nbsp;
+                        <input
+                        type='text'
+                        name='city'
+                        placeholder='Enter City'
+                        onChange={(e)=>{this.setState({city:e.target.value})}}
+                        />
+                    </div>
+                    <br/>
+                    <div>
+                    <input 
+                    type='submit' 
+                    value='Search' 
+                    className='btn btn-block'
+                    onClick={() => this.submit()}
+                     />
+                    </div>
+                    
+                </div>
+                <br/>
+                <div>
+                    <div>
+                        Weather Result
+                    </div>
+                    <br/>
+                    <br/>
+                    <div className="col-md-12">
+                        <label>Searched City </label>&nbsp;{this.state.result.name}
+                        <br/>
+                        <div className="col-md-6">Current temperature : 
+                            <span>{ this.state.result.main.temp }°c</span>
+                        </div>
+                        <br/>
+                        <div className="col-md-6">Feels Like : 
+                            <span>{ this.state.result.main.feels_like }°c</span>
+                        </div>
+                        <br/>
+                        <div className="col-md-6">Humidity : 
+                            <span>{ this.state.result.main.humidity }%</span>
+                        </div>
+                        <br/>
+                        <div className="col-md-6">Pressure : 
+                            <span>{ this.state.result.main.pressure }mBar</span>
+                        </div>
+                        <br/>
+                        <div className="col-md-6">Max. temperature : 
+                            <span>{ this.state.result.main.temp_max }°c</span>
+                        </div>
+                        <br/>
+                        <div className="col-md-6">Min. temperature : 
+                            <span>{ this.state.result.main.temp_min }°c</span>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+// import { useState } from "react";
+
+// const Search = ({onAdd}) =>{
+
+//     const [text, setText] = useState('');
+//     const onSubmit = (e) => {
+//         e.preventDefault()
+    
+//         if (!text) {
+//           alert('Please add a task')
+//           return
+//         }
+    
+//         onAdd({ text })
+    
+//         setText('')
+//       }
+
+//     return (
+//         <form className='add-form' onSubmit={onSubmit}>
+//           <div className='form-control'>
+//             <label>Task</label>
+//             <input
+//               type='text'
+//               placeholder='Enter City'
+//               value={text}
+//               onChange={(e) => setText(e.target.value)}
+//             />
+//           </div>
+    
+//           <input type='submit' value='Show Result' className='btn btn-block' />
+//         </form>
+//       )
+// }
